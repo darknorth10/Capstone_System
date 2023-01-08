c8546db24a73b755cba52753b019e611d2c8b661
darknorth10 <abbyalbus123@gmail.com>
2023-01-08 22:59:52 +0800

    usermanagement 50%

--- UserManagement/views.py
+++ UserManagement/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from LoginAuthentication.models import CustomUser
 
 # Create your views here.
 def user_management(request):
-  return render(request, 'UserInterface/user_management.html')
+  return render(request, 'UserInterface/user_management.html',
+    context = { 'Users': CustomUser.objects.all(), }
+  )
--- static/js/script.js
+++ static/js/script.js
@@ -6,18 +6,20 @@ $(document).ready(function() {
     if (shrink == false) {
       $('.sidenav').css('width', "60px");
       $('.sidenav-menu li span').hide();
-      $('.sidenav-menu li i').css("font-size", "1.1rem");
+      $('.sidenav-menu li i').css("font-size", "0.8rem");
+      $('.sidenav-menu li i').css("margin-bottom", "5px");
       $('#shrink').css('transform', 'rotate(180deg)');
       shrink = true;
     } else {
-      $('.sidenav').css('width', "230px");
+      $('.sidenav').css('width', "210px");
       setTimeout(function() {
         $('.sidenav-menu li span').show();
       }, 500);
-      $('.sidenav-menu li i').css("font-size", "0.9rem");
+      $('.sidenav-menu li i').css("font-size", "0.7rem");
       $('#shrink').css('transform', 'rotate(0deg)');
       shrink = false;
     }
     
   })
 });
+
